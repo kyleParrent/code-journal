939b2b8dfbe2afe63efdd6a7e04283096c7d6ce3
Kyle Parrent <kppp433@gmail.com>
2022-04-05 05:26:49 +0000

    Can push a value.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 /* global data */
 /* exported data */
+
+var img = document.querySelector('img');
+var userImage = document.querySelector('.photo');
+var userTitle = document.querySelector('.user-title');
+var userNotes = document.querySelector('.user-notes');
+var saveB = document.querySelector('.save');
+
+function savingPic(event) {
+  img.src = userImage.value;
+}
+
+function savingInfo(event) {
+  var store = {};
+  store.images = userImage.value;
+  store.title = userTitle.value;
+  store.notes = userNotes.value;
+  store.Id = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(store);
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascipt-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', savingInfo);
+userImage.addEventListener('input', savingPic);
+saveB.addEventListener('submit', savingInfo);
